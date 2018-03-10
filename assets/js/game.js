eb156fb05378736e03d9392af775b50b21371294
David Adeboye <david.adeboye@outlook.com>
2018-03-10 12:01:31 +0000

    Added background music

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,17 @@
 
+var backgroundAudio;
+
 
 function initGame() {
-  alert("This worked");
+
+	backgroundAudio = new Audio( 'assets/sounds/greenland.mp3' );
+	backgroundAudio.volume = 0.3;
+	//Adding loop
+	backgroundAudio.addEventListener('ended', function() {
+		this.currentTime = 0;
+		this.play();
+	}, false);
+	backgroundAudio.play();
+
+
 }
