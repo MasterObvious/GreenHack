c9f7d9d349c24cdd34ea3545a46b2dc042689e6d
Ben Curnow <ben@bencurnow.me.uk>
2018-03-10 15:55:20 +0000

    add research box

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -21,3 +21,27 @@ function placeCity(cityName, x, y){
 	$city.prependTo("#map_container");
 	$city.attr("id", cityName);
 }
+
+function updateResearch() {
+	$('#research-list').empty();
+	jsonData.research.reverse().forEach(function(el) {
+		if (!(el in stateCurrentResearch)) {
+			let $researchBlock = $("#research-template").clone();
+			$researchBlock.removeClass('displaynone');
+			$researchBlock.prependTo('#research-list');
+			$researchBlock.find('.research-name').html(el.name);
+			$researchBlock.find('.research-price').html(el.price);
+			$researchBlock.find('.research-station-cost').html(el.station_cost);
+			$researchBlock.find('.research-pollution').html(el.pollution);
+		}
+	})
+}
+
+function closeResearch() {
+	$('#research_container').addClass('displaynone');
+}
+
+function openResearch() {
+	updateResearch();
+	$('#research_container').removeClass('displaynone');
+}
